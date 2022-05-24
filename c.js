08f55ac662eb209b2550972127c3f4f17764297d
eunekim <keh8992@gmail.com>
2022-05-24 18:57:43 +0900

    동적 element 생성     container.appendChild(btn1);ontainer

--- c.js
+++ c.js
@@ -4,13 +4,30 @@ function showConsole() {
     console.log(btn);
 } */
 
-const input = document.getElementById('a');
-input.addEventListener('change', getInput)
+const body = document.querySelector('body');
+const btn = document.querySelector('.b')
+const inputElem = document.querySelector('#a');
+
+let value = "";
+
+btn.addEventListener('click', getInput);
 
 function getInput(event) {
-    //console.log(event.target.value);
-    document.getElementById('c').innerHTML=event.target.value;
-    input.value = "";
+    value = inputElem.value;
+    const textElem = document.createElement('span');
+    const container = document.createElement('div');
+    const btn1 = document.createElement('button');
+
+    btn1.innerHTML = '삭제';
+    textElem.innerHTML = value;
+    
+    //const sum = container.appendChild(textElem,btn1);
+
+    container.appendChild(textElem);
+    container.appendChild(btn1);
+    body.appendChild(container);
+
+    inputElem.value = "";
 }
 
 
